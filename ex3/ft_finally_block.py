7cd669ae493f4f8d0dc8c7998c98b17d92143c73
safaaquran <smalquran22@cit.just.edu.jo>
2026-03-11 01:27:37 +0300

    added

--- ex3/ft_finally_block.py
+++ ex3/ft_finally_block.py
@@ -1,3 +1,31 @@
 #!/usr/bin/env python3
 
+def water_plants(plant_list: str) -> None:
+    print("Opening watering system")
+    try:
+        for plant in plant_list:
+            if plant == None:
+                raise Exception("Cannot water None- invalid plant!")
+            print(f"Watering {plant}")
 
+    except Exception as e:
+        print(f"Error: {e}")
+
+    finally:
+        print("Closing watering system (cleanup)")
+
+def test_watering_system():
+    valid_values = ["tomato", "lettuce", "carrots"]
+    print("Testing normal watering...")
+    water_plants(valid_values)
+    print("Watering completed successfully!\n")
+
+    invalid_values = ["tomato", None, "carrots"]
+    print("Testing with error...")
+    water_plants(invalid_values)
+
+
+if __name__ == "__main__":
+    print("=== Garden Watering System ===\n")
+    test_watering_system()
+    print("\n Cleanup always happens, even with errors!")
